9ea26fc0ae8ab4ad54df54966327ae624c63d09d
tarzander50 <xandermorehouse50@gmail.com>
2013-12-10 00:45:00 -0500

    input handling

--- shortestpaths.cpp
+++ shortestpaths.cpp
@@ -1,11 +1,51 @@
 #include <iostream>
 #include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <map>
+#include <stdlib.h>
 using namespace std;
+
+const char COMMENT = '#';
+const string DIRECTED = "D";
+const string UNDIRECTED = "UD";
 
 int main(int argc, char* argv[])
 {
+  ifstream input;
+  char tmpstr[80];
+  string gtype;
+  map<char, map<char, int> > adj; // <from <to, weight> >
+  
+  if (argc != 1)
+  {
+    printf("Must include .txt file as command line arg!");
+    exit(1);
+  }
+  input.open(argv[1]);
+  while (input.peek() == COMMENT)	// removing comment lines
+    input.getline(tmpstr, 80);
+  input >> gtype;
+  
   printf("Shortest paths program!\n");
 
+  while (!input.eof())
+  {
+    char f, t;
+    int w;
+    input >> f;
+    input >> t;
+    input >> w;
+    
+    adj[f][t] = w;
+    if (gtype == UNDIRECTED)
+    {
+      adj[t][f] = w;
+    }
+  }
 
+  input.close();
   return 0;
 }
